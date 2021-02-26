ad8baa021beba3f6632df17d9af776cb3cf5e69e
tmdawson23 <78229709+tmdawson23@users.noreply.github.com>
2021-02-26 16:35:00 -0600

    snowman lab

--- Program.cs
+++ Program.cs
@@ -50,8 +50,14 @@ class Program
             /*Step 1 update ValidMenuChoice to return true if the user 
             entered 1, 2 or 3 and return false if they entered anything else.
             */
-
-
+            if(userInput == "1" || userInput == "2" || userInput == "3")
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
         }
 
         public static void Route(string userInput, ref int gamesWon, ref int gamesLost)
@@ -59,9 +65,14 @@ class Program
             /*Step 2: Update to call Snowman if the user entered 1 and 
              * ScoreBoard if they entered 2
              */
-
-
-
+            if (userInput == "1")
+            {
+                SnowMan(ref gamesWon, ref gamesLost);
+            }
+            else if (userInput == "2")
+            {
+                ScoreBoard(gamesWon, gamesLost);
+            }
         }
 
         public static void SnowMan(ref int gamesWon, ref int gamesLost)
@@ -104,8 +115,32 @@ class Program
              * found, to press a key to continue.  Update the guessed letters array 
              * and clear the console. 
              */
+            if (guessed == "No Letters Guessed Yet")
+            {
+                guessed = "";
+            }
 
+            guessed += pickedLetter;
+            int indexFound = -1;
 
+            for(int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == pickedLetter)
+                {
+                    displayWord[i] = pickedLetter;
+                    indexFound = i;
+                
+                }
+            }
+
+            if (indexFound == -1)
+            {
+                Console.WriteLine("Letter was not found");
+                Console.WriteLine("Press any key to continue.....");
+                Console.ReadKey();
+                missed ++;
+                Console.Clear();
+            }
         }
 
         public static bool KeepGoing(char[] displayWord, int missed)
@@ -114,9 +149,18 @@ class Program
              * AND there are still underscores left meaning they have not 
              * fully guessed the word
              */
-
-
-
+            if (missed >6)
+            {
+                return false;
+            }
+            for (int i = 0; i < displayWord.Length; i++)
+            {
+                if (displayWord[i]== '_') 
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public static void ShowBoard(char[] displayWord, int missed, string guessed)
@@ -139,18 +183,21 @@ class Program
             /*SetDisplayWord to return a character array of 
              * underscores and spaces to match the word returned in step 3
              */
-
-
-
+            char [] array = new char[word.Length];
+            for (int i = 0; i< word.Length; i++)
+            {
+                array [i] = '_';
+            }
+            return array;
         }
         public static string GetRandomWord()
         {
             /* Return a random word to be used in the game
              */
             string[] words = { "ROLLTIDE", "ALABAMA", "TUSCALOOSA", "UNIVERSITY", "SCHOOL" };
-
-
-
+            Random rnd = new Random();
+            int randWord = rnd.Next(0,4);
+            return words[randWord];
         }
         public static void ScoreBoard(int gamesWon, int gamesLost)
         {
